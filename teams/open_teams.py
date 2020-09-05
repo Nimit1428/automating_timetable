cb46a770f369c48b6c7ec17bc7ad290859633717
shreyaparadkar <srparadkar15@gmail.com>
2020-09-05 15:58:58 +0530

    basic automation added

--- teams/open_teams.py
+++ teams/open_teams.py
@@ -1,50 +1,43 @@
 import pyautogui as pag
 import time
+from pw import email,password
 
 def open_teams():
-    #196,748 start menu to open teams
-    loc=pag.locateOnScreen('start.png')
+
+    #start menu
+    loc=pag.locateOnScreen('./images/start.png')
     center=pag.center(loc)
     pag.moveTo(x=center.x,y=center.y)
     pag.click(button='left', clicks=1)
     pag.write('teams',interval=1)
     pag.press('enter')
-    time.sleep(3)
+    time.sleep(8)
 
     #enter email
-    # loc=pag.locateOnScreen('email.png')
-    # print(loc)
-    # print(pag.center(loc))
-    # center=pag.center(loc)
-    # pag.moveTo(x=center.x,y=center.y)
-    # pag.click(button='left',clicks=1)
-
-
-    pag.write('email')
+    pag.write(email)
     pag.press('enter')
 
     #enter password
-    time.sleep(4)
-    pag.write('password')
+    time.sleep(8)
+    pag.write(password)
     pag.press('enter')
-    time.sleep(2)
-
+    time.sleep(3)
 
     #check for popups
-    # loc=pag.locateOnScreen('allow_pemissions.png')
-    # if(loc):
-    #     print(loc)
-    #     print(pag.center(loc))
-    #     center=pag.center(loc)
-    #     pag.moveTo(x=center.x,y=center.y)
-    #     pag.click(button='left',clicks=1)
-    #     loc=pag.locateOnScreen('signin.png')
-    #     print(loc)
-    #     print(pag.center(loc))
-    #     center=pag.center(loc)
-    #     pag.moveTo(x=center.x,y=center.y)
-    #     pag.click(button='left',clicks=1)
-    #     time.sleep(1)
+    loc=pag.locateOnScreen('./images/allow_permissions.png')
+    if(loc):
+        print(loc)
+        print(pag.center(loc))
+        center=pag.center(loc)
+        pag.moveTo(x=center.x,y=center.y)
+        pag.click(button='left',clicks=1)
+        loc=pag.locateOnScreen('./images/signin.png')
+        print(loc)
+        print(pag.center(loc))
+        center=pag.center(loc)
+        pag.moveTo(x=center.x,y=center.y)
+        pag.click(button='left',clicks=1)
+        time.sleep(1)
 
     #maximize screen
     pag.keyDown('alt')
@@ -52,21 +45,25 @@ def open_teams():
     pag.press('x')
     pag.keyUp('alt')
     pag.press('enter')
-    time.sleep(1)
+    time.sleep(10)
     join_class()
 
+#joining class
+#add images of all class. sirf logo rakhna text nai
 def join_class():
-    loc=pag.locateOnScreen('trial.png')
+    time.sleep(2)
+    loc=pag.locateOnScreen('./images/trial.png')
     print(loc)
     print(pag.center(loc))
     center=pag.center(loc)
     pag.moveTo(x=center.x,y=center.y)
     pag.click(button='left',clicks=1)
-    time.sleep(1)
+    time.sleep(3)
     join_meeting()
 
+#join perticular meeting in class
 def join_meeting():
-    loc=pag.locateOnScreen('join.png')
+    loc=pag.locateOnScreen('./images/join.png')
     if(loc):
         print(loc)
         print(pag.center(loc))
@@ -74,6 +71,7 @@ def join_meeting():
         pag.moveTo(x=center.x,y=center.y)
         pag.click(button='left',clicks=1)
 
+    #maximize the screen
     pag.keyDown('alt')
     pag.press(' ')
     pag.press('x')
@@ -82,7 +80,8 @@ def join_meeting():
 
     time.sleep(3)
 
-    loc=pag.locateOnScreen('audio.png')
+    #mute audio
+    loc=pag.locateOnScreen('./images/audio.png')
     if(loc):
         print(loc)
         print(pag.center(loc))
@@ -90,7 +89,8 @@ def join_meeting():
         pag.moveTo(x=center.x,y=center.y)
         pag.click(button='left',clicks=1)
 
-    loc=pag.locateOnScreen('video.png')
+    #turn off video
+    loc=pag.locateOnScreen('./images/video.png')
     if(loc):
         print(loc)
         print(pag.center(loc))
@@ -98,13 +98,14 @@ def join_meeting():
         pag.moveTo(x=center.x,y=center.y)
         pag.click(button='left',clicks=1)
     
-    loc=pag.locateOnScreen('join_now.png')
+    #join meetin now
+    loc=pag.locateOnScreen('./images/join_now.png')
     center=pag.center(loc)
     pag.moveTo(x=center.x,y=center.y)
     pag.click(button='left',clicks=1)
     stay_online()
 
-
+#sleep mode me na jai esliye
 def stay_online():
     duration=10
     try:
@@ -116,16 +117,19 @@ def stay_online():
     except KeyboardInterrupt:
         print('stopping...')
 
+#exit meeting 
 def leave_meeting():
-    loc=pag.locateOnScreen('leave.png')
+    loc=pag.locateOnScreen('./images/leave.png')
     center=pag.center(loc)
     pag.moveTo(x=center.x,y=center.y)
     pag.click(button='left',clicks=1)
     time.sleep(1)
     teams_screen()
 
+
+#go to main teams home screen
 def teams_screen():
-    loc=pag.locateOnScreen('teams.png')
+    loc=pag.locateOnScreen('./images/teams.png')
     center=pag.center(loc)
     pag.moveTo(x=center.x,y=center.y)
     pag.click(button='left',clicks=1)
@@ -134,5 +138,4 @@ open_teams()
 # join_class()
 # join_meeting()
 # teams_screen()
-
 
