696d36d99dd9df8deb271e8b7efa219be7a97dda
nimit1428 <mnimitsavant@outlook.com>
2020-09-05 23:20:17 +0530

    smh

---
+++ teams/tempCodeRunnerFile.py
@@ -0,0 +1,25 @@
+  # #enter email
+    # pag.write(email)
+    # pag.press('enter')
+
+    # #enter password
+    # time.sleep(8)
+    # pag.write(password)
+    # pag.press('enter')
+    # time.sleep(3)
+
+    # #check for popups
+    # loc=pag.locateOnScreen('./images/allow_permissions.png')
+    # if(loc):
+    #     print(loc)
+    #     print(pag.center(loc))
+    #     center=pag.center(loc)
+    #     pag.moveTo(x=center.x,y=center.y)
+    #     pag.click(button='left',clicks=1)
+    #     loc=pag.locateOnScreen('./images/signin.png')
+    #     print(loc)
+    #     print(pag.center(loc))
+    #     center=pag.center(loc)
+    #     pag.moveTo(x=center.x,y=center.y)
+    #     pag.click(button='left',clicks=1)
+    #     time.sleep(1)
